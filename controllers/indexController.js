88d1bd7b66a66627c1b4d02cdf9d9bc21339f012
bethmenzies <bethmenzies@btinternet.com>
2024-05-23 17:02:44 +0100

    use controller for index

---
+++ controllers/indexController.js
@@ -0,0 +1,6 @@
+const asyncHandler = require("express-async-handler");
+
+// Display Home page.
+exports.home = asyncHandler(async (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
